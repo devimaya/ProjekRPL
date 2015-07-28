286bd6fabce7b7c57e777e653cfaea8c44c3497d
Devi <evie.oath@gmail.com>
2015-07-28 17:32:57 +0700

    delete
    contohnya kekopi

--- 1_code/website_code/chart.php
+++
@@ -1,8 +0,0 @@
-
-
-<?php
-include_once 'ofc-library/open_flash_chart_object.php';
-open_flash_chart_object(1300, 600, 'http://'. $_SERVER['SERVER_NAME'] .'/~13group5/demo2/chart-data.php', false );
-?>
-
-
